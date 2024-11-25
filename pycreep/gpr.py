957e0e504b3729d335a80d4793705935e5f8dbae
Mark Messner <mark.messner@gmail.com>
2024-11-25 17:38:18 -0600

    Not working

--- pycreep/gpr.py
+++ pycreep/gpr.py
@@ -5,160 +5,91 @@ import numpy as np
 
 import scipy.stats as ss
 
+from math import log
+
 import torch
 import pyro
-from torch.distributions import constraints
-from pyro.contrib import gp
-from pyro.contrib.gp.kernels.kernel import Kernel
-from pyro.nn.module import PyroParam, PyroSample
 import pyro.distributions as dist
+from pyro.infer.autoguide import AutoDelta
+import pyro.optim as optim
+from pyro.infer import SVI, Trace_ELBO
+
+from pyro.nn import PyroModule, PyroSample
 
 from pycreep import ttp
 
 from tqdm import trange
 
 
-class LMKernel(Kernel):
-    """Kernel used for Gaussian process LM regression
+class UncenteredLMP(PyroModule):
+    def __init__(self, C_mean=20.0, C_variance=0.1):
+        super().__init__()
+        self.C_mean = PyroSample(dist.LogNormal(log(C_mean), 1.0))
+        self.C_variance = PyroSample(dist.LogNormal(log(C_variance), 1.0))
 
-    Keyword Args:
-        lengthscale (torch.tensor): kernel length scale
-        variance (torch.tensor): kernel variance
-        C_mean (torch.tensor): initial guess at the mean of the Larson-Miller parameter
-        C_variance (torch.tensor): initial guess at the variance of the Larson-Miller parameter
+    def forward(self, X):
+        return (
+            X[:, 0] * (self.C_mean + torch.log10(X[:, 1])),
+            X[:, 0] ** 2.0 * self.C_variance,
+        )
 
-    Input order for parameters in X is [temperature, time, heat_index]
 
-    -1 indicates we want to use the all heat average, rather than heat-specific information
-    """
+class Kernel(PyroModule):
+    def __init__(self, var=0.01, length=1.0):
+        super().__init__()
+        self.var = PyroSample(dist.LogNormal(log(var), 1.0))
+        self.length = PyroSample(dist.LogNormal(log(length), 1.0))
 
-    def __init__(
-        self,
-        lengthscale=torch.tensor(1.0),
-        variance=torch.tensor(0.01),
-        C_mean=torch.tensor(20.0),
-        C_variance=torch.tensor(1.0),
-    ):
-        super().__init__(3, None)
-
-        self.lengthscale = PyroParam(lengthscale, constraints.positive)
-        self.variance = PyroParam(variance, constraints.positive)
-        self.C_mean = PyroParam(C_mean, constraints.positive)
-        self.C_variance = PyroParam(C_variance, constraints.positive)
-
-    def _d2(self, X, Z):
-        """
-        Squared distance calculator, cribbed from the base pyro source
-        """
-        X = self._slice_input(X)
-        Z = self._slice_input(Z)
-
+    def distance(self, X, Z):
+        X = X.unsqueeze(1)
+        Z = Z.unsqueeze(1)
         X2 = (X**2.0).sum(1, keepdim=True)
         Z2 = (Z**2.0).sum(1, keepdim=True)
         XZ = X.matmul(Z.t())
         r2 = X2 - 2 * XZ + Z2.t()
         return r2.clamp(min=0)
 
-    def forward(self, X, Z=None, diag=False):
-        """
-        Forward method for the kernel
-        """
-        if Z is None:
-            Z = X
+    def forward(self, X, Y, scov):
+        scaled_r2 = torch.exp(-0.5 * self.distance(X, Y) / (scov + self.length))
+        sf = self.var / torch.sqrt(torch.abs(1.0 + scov / self.length))
 
-        mean_X, var_X = self.calculate_sample_statistics(X)
-        mean_Z, var_Z = self.calculate_sample_statistics(Z)
-
-        scov = torch.outer(var_X, var_Z)
-
-        scaled_r2 = torch.exp(
-            -0.5 * self._d2(mean_X, mean_Z) / (scov + self.lengthscale)
-        )
-        sf = self.variance / torch.sqrt(torch.abs(1.0 + scov / self.lengthscale))
-
-        mat = sf * scaled_r2
-
-        if diag:
-            return mat.diag()
-        return mat
+        return sf * scaled_r2
 
 
-class UncenteredLMKernel(LMKernel):
-    """
-    Kernel used for Gaussian process LM regression, no lot centering
+class GPModel(PyroModule):
+    def __init__(self, kernel, ttp_model, noise=0.001, jitter=1.0e-6):
+        super().__init__()
+        self.kernel = kernel
+        self.ttp_model = ttp_model
+        self.noise = PyroSample(dist.LogNormal(log(noise), 1.0))
+        self.jitter = jitter
 
-    Keyword Args:
-        lengthscale (torch.tensor): kernel length scale
-        variance (torch.tensor): kernel variance
-        C_mean (torch.tensor): initial guess at the mean of the Larson-Miller parameter
-        C_variance (torch.tensor): initial guess at the variance of the Larson-Miller parameter
+    def crossvariance(self, X1, X2, noise=True):
+        C1_mean, C1_variance = self.ttp_model(X1)
+        C2_mean, C2_variance = self.ttp_model(X2)
+        scov = torch.outer(C1_variance, C2_variance)
+        k = self.kernel(C1_mean, C2_mean, scov)
+        if noise:
+            return k + (self.noise + self.jitter) * torch.eye(X1.shape[0])
+        return k
 
-    Input order for parameters in X is [temperature, time, heat_index]
+    def forward(self, X1, y, X2=None):
+        if X2 is None:
+            X2 = X1
 
-    -1 indicates we want to use the all heat average, rather than heat-specific information
-    """
+        k = self.crossvariance(X1, X2)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        Lff = torch.linalg.cholesky(k)
 
-    def calculate_sample_statistics(self, X):
-        """
-        Calculate the lot statistics for each sample
-
-        Args:
-            X (torch.tensor): input data, shape (N,3)
-        """
-        mean_X = X[:, 0] * (self.C_mean + torch.log10(X[:, 1]))
-        var_X = X[:, 0] ** 2.0 * self.C_variance
-
-        return mean_X, var_X
-
-
-class LotCenteredLMKernel(LMKernel):
-    """
-    Kernel used for Gaussian process LM regression with lot centering via a hierarchical model
-
-    Args:
-        nheat (int): number of heats
-
-    Keyword Args:
-        lengthscale (torch.tensor): kernel length scale
-        variance (torch.tensor): kernel variance
-        C_mean (torch.tensor): initial guess at the mean of the Larson-Miller parameter
-        C_variance (torch.tensor): initial guess at the variance of the Larson-Miller parameter
-
-    Input order for parameters in X is [temperature, time, heat_index]
-
-    -1 indicates we want to use the all heat average, rather than heat-specific information
-    """
-
-    def __init__(self, nheat, *args, subvariance=torch.tensor(0.1), **kwargs):
-        super().__init__(*args, **kwargs)
-        self.nheat = nheat
-        self.subvariance = subvariance
-
-        self.lot_C_mean = PyroSample(
-            dist.Normal(self.C_mean, self.C_variance).expand([nheat]).to_event(1)
-        )
-        self.lot_C_variance = PyroSample(
-            dist.HalfNormal(self.subvariance).expand([nheat]).to_event(1)
+        pyro.sample(
+            "y",
+            dist.MultivariateNormal(torch.zeros(X1.shape[0]), scale_tril=Lff)
+            .expand_by(y.shape[:-1])
+            .to_event(y.dim() - 1),
+            obs=y,
         )
 
-    def calculate_sample_statistics(self, X):
-        """
-        Calculate the lot statistics for each sample
-
-        Args:
-            X (torch.tensor): input data, shape (N,3)
-        """
-        inds = X[:, 2].long()
-        mean = torch.where(inds == -1, self.C_mean, self.lot_C_mean[inds])
-        var = torch.where(inds == -1, self.C_variance, self.lot_C_variance[inds])
-
-        mean_X = X[:, 0] * (mean + torch.log10(X[:, 1]))
-        var_X = X[:, 0] ** 2.0 * var
-
-        return mean_X, var_X
+        return k
 
 
 class GPRLMPModel(ttp.TTPAnalysis):
@@ -198,7 +129,7 @@ class GPRLMPModel(ttp.TTPAnalysis):
         self,
         *args,
         uncentered=False,
-        noise=0.01,
+        noise=0.001,
         niter=500,
         temperature_scale=1000.0,
         **kwargs,
@@ -232,7 +163,7 @@ class GPRLMPModel(ttp.TTPAnalysis):
             verbose (bool):     print out the training progress
         """
         # Set up the input data in order
-        X = torch.tensor(
+        self.X = torch.tensor(
             np.stack(
                 [
                     self.temperature / self.temperature_scale,
@@ -242,26 +173,18 @@ class GPRLMPModel(ttp.TTPAnalysis):
                 axis=1,
             )
         )
-        y = torch.log10(torch.tensor(self.stress))
+        self.y = torch.log10(torch.tensor(self.stress))
 
         # Setup optimizer
         pyro.clear_param_store()
-        self.kernel = (
-            UncenteredLMKernel()
-            if self.uncentered
-            else LotCenteredLMKernel(len(self.heat_indices))
-        )
 
-        self.gp = gp.models.GPRegression(X, y, self.kernel, torch.tensor(self.noise))
-        optimizer = torch.optim.Adam(self.gp.parameters(), lr=1.0e-2)
-        loss_fn = pyro.infer.Trace_ELBO().differentiable_loss
+        kernel = Kernel()
+        ttp_model = UncenteredLMP()
+        self.model = GPModel(kernel, ttp_model, noise=self.noise)
 
-        # Closure for optimizer
-        def closure():
-            optimizer.zero_grad()
-            loss = loss_fn(self.gp.model, self.gp.guide)
-            loss.backward(retain_graph=True)
-            return loss
+        optimizer = optim.Adam({"lr": 0.01})
+        self.guide = AutoDelta(self.model)
+        svi = SVI(self.model, self.guide, optimizer, loss=Trace_ELBO())
 
         # Optimize
         if verbose:
@@ -271,12 +194,12 @@ class GPRLMPModel(ttp.TTPAnalysis):
             iter = range(self.niter)
 
         for _ in iter:
-            loss = optimizer.step(closure)
+            loss = svi.step(self.X, self.y)
 
             if verbose:
                 iter.set_description("loss=%e" % loss)
 
-        self.C_avg = self.gp.kernel.C_mean.item()
+        self.C_avg = pyro.param("AutoDelta.ttp_model.C_mean").item()
 
         return self
 
@@ -291,7 +214,7 @@ class GPRLMPModel(ttp.TTPAnalysis):
         if np.isscalar(temperature):
             temperature = np.full_like(time, temperature)
 
-        X = torch.tensor(
+        Xp = torch.tensor(
             np.stack(
                 [
                     temperature / self.temperature_scale,
@@ -302,10 +225,21 @@ class GPRLMPModel(ttp.TTPAnalysis):
             )
         )
 
-        with torch.no_grad():
-            mean, var = self.gp(X, full_cov=False, noiseless=False)
+        k11 = self.guide(self.X, self.y, X2=self.X)
+        k21 = self.guide(self.X, self.y, X2=Xp)
+        k22 = self.guide(Xp, self.y, X2=Xp)
 
-        return mean.numpy(), var.numpy()
+        (
+            LU,
+            pivots,
+        ) = torch.linalg.lu_factor(k11)
+
+        mean = k21.matmul(
+            torch.linalg.lu_solve(LU, pivots, self.y.unsqueeze(-1))
+        ).squeeze(-1)
+        variance = k22 - k21.matmul(torch.linalg.lu_solve(LU, pivots, k21.t()))
+
+        return mean.numpy(), np.diag(variance.numpy())
 
     def predict_stress(self, time, temperature, confidence=None):
         """
